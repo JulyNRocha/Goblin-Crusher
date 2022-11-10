973c5691eef2699cc72bad70d481aa6c54cdd908
July Rocha <nrochajuly@gmail.com>
2022-11-10 00:15:44 -0300

    :sparkles: Make enemy pass through path

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitSeconds = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitSeconds);
+        }
+    }
+}
