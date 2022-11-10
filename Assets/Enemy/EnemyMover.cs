091306d1a49d9d549bbffdcabedefe9aa22caa6a
July Rocha <nrochajuly@gmail.com>
2022-11-10 18:06:52 -0300

    :sparkles: Reset Enemy Health and reset Object into the pool

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -8,7 +8,7 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     [SerializeField] [Range(0f,5f)] float speed = 1f;
 
-    void Start()
+    void OnEnable()
     {
         FindPath();
         ReturnToStart();
