a53d43479f29e1a79bea1043e0a98b259214360e
July Rocha <nrochajuly@gmail.com>
2022-11-10 02:20:16 -0300

    :sparkles: Make cannons appear when click

--- Assets/Tile/Waypoint.cs
+++ Assets/Tile/Waypoint.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject cannonPrefab;
     [SerializeField] bool isPlacable;
 
     void OnMouseDown()
     {
         if (isPlacable)
         {
+            Instantiate(cannonPrefab, transform.position, Quaternion.identity);
             Debug.Log(transform.name);
         }
     }
