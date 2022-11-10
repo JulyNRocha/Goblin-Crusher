0028b1d05f6cfc731698964f39097fc41aacfaf2
July Rocha <nrochajuly@gmail.com>
2022-11-10 18:26:10 -0300

    :sparkles: Instantiates goblins with in the ObjectPool

--- Assets/Enemy/ObjectPool.cs
+++ Assets/Enemy/ObjectPool.cs
@@ -7,13 +7,18 @@ public class ObjectPool : MonoBehaviour
 {
     [SerializeField] GameObject goblingPrefab;
     [SerializeField] int poolSize = 5;
-    [SerializeField] float spawnTime;
+    [SerializeField] float spawnTime = 1f;
 
     GameObject[] pool;
 
     void Awake()
     {
         PopulatePool();        
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnGoblin());
     }
 
     void PopulatePool()
@@ -27,16 +32,24 @@ public class ObjectPool : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        StartCoroutine(SpawnGoblin());
-    }
-
     IEnumerator SpawnGoblin()
     {
         while(true)
         {
+            EnabledObjectInPool();
             yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+    void EnabledObjectInPool()
+    {
+        foreach(GameObject enemy in pool)
+        {
+            if (!enemy.activeInHierarchy)
+            {
+                enemy.SetActive(true);
+                return;
+            }
         }
     }
 }
