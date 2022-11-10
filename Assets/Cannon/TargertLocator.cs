c3ccc3f22cc600b8cba06c4ec1137dfa8ff6c18a
July Rocha <nrochajuly@gmail.com>
2022-11-10 20:36:19 -0300

    :sparkles: Find closest target

--- Assets/Cannon/TargertLocator.cs
+++ Assets/Cannon/TargertLocator.cs
@@ -7,16 +7,31 @@ public class TargertLocator : MonoBehaviour
 {
     [SerializeField] Transform cannonHead;
     Transform target;
-    
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemys = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemys)
+        {
+            float targetDistance = Vector3.Distance(this.transform.position, enemy.transform.position);
+
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
