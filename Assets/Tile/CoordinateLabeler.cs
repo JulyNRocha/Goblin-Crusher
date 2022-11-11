f0fd136f2cb294fa76de89dab45150517eb3df91
July Rocha <nrochajuly@gmail.com>
2022-11-11 17:45:09 -0300

    🔨 Add RequireComponent TMPro to CoordinateLabeler script

--- Assets/Tile/CoordinateLabeler.cs
+++ Assets/Tile/CoordinateLabeler.cs
@@ -5,6 +5,7 @@ using TMPro;
 using System;
 
 [ExecuteAlways]
+[RequireComponent(typeof(TextMeshPro))]
 public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defautColor = Color.black;
