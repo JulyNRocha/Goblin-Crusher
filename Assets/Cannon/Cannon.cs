a1c04e2e2c5f64e36e459756b28547a7c6d8f6bc
July Rocha <nrochajuly@gmail.com>
2022-11-21 14:21:06 -0300

    :sparkles: Make Build Time

--- Assets/Cannon/Cannon.cs
+++ Assets/Cannon/Cannon.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Cannon : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float waitDelay = 1f;
 
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
     public bool CreateCannon(Cannon cannon, Vector3 position)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -20,5 +25,27 @@ public class Cannon : MonoBehaviour
         }
         
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(waitDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
